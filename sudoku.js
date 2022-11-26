cc780a0f0c9d25ea9f872de2873a7a8d4abeaa04
Nickolay-Klimentov <moody.klmn@icloud.com>
2022-11-26 20:36:27 +0300

    add function fillNumber: doesnt work correct

--- sudoku.js
+++ sudoku.js
@@ -1,65 +1,41 @@
-/**
- * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
- * Возвращает игровое поле после попытки его решить.
- * Договорись со своей командой, в каком формате возвращать этот результат.
- */
+// используй дальше board
 
-function solve(boardString) {
-  const board = [];
-  let arrBoard = boardString.split("");
-  for (let i = 0; i < arrBoard.length; i += 9) {
-    const check = arrBoard.slice(i, i + 9);
-    board.push(check);
-  }
+//  функция ищет первую попавшуюся чёрточку и возвращает её индекс
+function findEmptyField(board) {
+  // переменная для результата
+  const arrPosition = [];
+  // перебор по массивам
+  for (let i = 0; i < board.length; i += 1) {
+    // поиск элемента с чёрточкой и возврат его индекса
+    const a = board[i].findIndex((el) => el === "-");
 
-  // используй дальше board
-
-  //  функция ищет перву попавшуюся чёрточку и возвращает её индекс
-  function findEmptyField(board) {
-    //переменная для результата
-    const arrPosition = [];
-    // перебор по массивам
-    for (let i = 0; i < board.length; i++) {
-      // поиск элемента с чёрточкой и возврат его индекса
-      a = board[i].findIndex((el) => el === "-");
-
-      if (a >= 0) {
-        //  тут я беру индекс массива в котором есть чёрточки
-        console.log(board[i]);
-        arrPosition.push(board.findIndex((el) => el === board[i]));
-        //  тут я добавляю индекс самой чёрточки
-        arrPosition.push(a);
-        // тут была проверка,что нам возвращает результат в консоль
-        // console.log(result);
-        console.log(arrPosition + "TEST");
-
-        return arrPosition;
-      }
+    if (a >= 0) {
+      //  тут я беру индекс массива в котором есть чёрточки
+      // console.log(board[i]);
+      arrPosition.push(board.findIndex((el) => el === board[i]));
+      //  тут я добавляю индекс самой чёрточки
+      arrPosition.push(a);
+      // тут была проверка,что нам возвращает результат в консоль
+      // console.log(result);
+      // console.log(arrPosition);
     }
   }
-
-  const position = findEmptyField(board);
-  console.table(board)
-
-  //принимает проверяемое число, его позицию (в виде массива или отдельно индексов)
-  //и саму доску. возвращает true или false в зависимости прошла проверка или нет
-
-  checkValidity(2, position, board);
+  return arrPosition;
 }
-
-// Проверяет повтор 1-9 в квадрате
-function checkValidity(number, position /*row, col*/, board) {
+// принимает проверяемое число, его позицию в виде массива и саму доску
+// возвращает true или false в зависимости прошла проверка или нет
+function checkValidity(number, position, board) {
   const [row, col] = position;
   const boardSize = 9;
-  //проверка наличия числа в строке
+  // проверка наличия числа в столбце
   for (let i = 0; i < boardSize; i += 1) {
     if (board[i][col] === number) return false;
   }
-  //проверка наличия числа в столбце
+  // проверка наличия числа в строке
   for (let j = 0; j < boardSize; j += 1) {
     if (board[row][j] === number) return false;
   }
-  //проверка наличия числа в блоке 3х3
+  // проверка наличия числа в блоке 3х3
   const blockRowStart = Math.floor(row / 3) * 3;
   const blockColStart = Math.floor(col / 3) * 3;
 
@@ -68,16 +44,44 @@ function checkValidity(number, position /*row, col*/, board) {
       if (board[i][j] === number) return false;
     }
   }
-  return true; //если все 3 проверки прошли то число подходит
+  return true; // если все 3 проверки прошли то число подходит
 }
 
+// функция заполняет пустые клетки цифрами
+function fillNumber(board) {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const number of numbers) {
+    const position = findEmptyField(board);
+    if (checkValidity(number, position, board) === true) {
+      const [i, j] = position;
+      board[i][j] = number;
+    } else fillNumber(board);
+  }
+  return board;
+}
+
+/**
+ * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
+ * Возвращает игровое поле после попытки его решить.
+ * Договорись со своей командой, в каком формате возвращать этот результат.
+ */
+function solve(boardString) {
+  const board = [];
+  const arrBoard = boardString.split("");
+  for (let i = 0; i < arrBoard.length; i += 9) {
+    const check = arrBoard.slice(i, i + 9);
+    board.push(check);
+  }
+  fillNumber(board);
+  console.log(board);
+  return board;
+}
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
