6946ea805cd0ffdd09f47fd7cd5f5ec5ecf14127
Mariia <pravdova97@mail.ru>
2022-11-27 02:54:57 +0300

    corrected function solve

--- sudoku.js
+++ sudoku.js
@@ -1,91 +1,128 @@
 const gradient = require("gradient-string");
 const tinycolor = require("tinycolor2");
 
-function solve(boardString) {
-  const board = [];
-  let arrBoard = boardString.split("");
-  for (let i = 0; i < arrBoard.length; i += 9) {
-    const check = arrBoard.slice(i, i + 9);
-    board.push(check);
-  }
-
-  // используй дальше board
-
-  //  функция ищет первую попавшуюся чёрточку и возвращает её индекс
-  function findEmptyField(board) {
-    //переменная для результата
-    const arrPosition = [];
-    // перебор по массивам
-    for (let i = 0; i < board.length; i++) {
-      // поиск элемента с чёрточкой и возврат его индекса
-      a = board[i].findIndex((el) => el === "-");
-
-      if (a >= 0) {
-        //  тут я беру индекс массива в котором есть чёрточки
-        console.log(board[i]);
-        arrPosition.push(board.findIndex((el) => el === board[i]));
-        //  тут я добавляю индекс самой чёрточки
-        arrPosition.push(a);
-        // тут была проверка,что нам возвращает результат в консоль
-        return arrPosition;
+function findEmptyField(arrBoard) {
+  for (let y = 0; y < 9; y += 1) {
+    for (let x = 0; x < 9; x += 1) {
+      if (arrBoard[y][x] === "-") {
+        return [y, x];
       }
     }
   }
-
-  const position = findEmptyField(board);
-  console.table(board);
+  return null;
 }
 
-// принимает проверяемое число, его позицию в виде массива и саму доску
-// возвращает true или false в зависимости прошла проверка или нет
-function checkValidity(number, position, board) {
-  const [row, col] = position;
+function checkValidity(position, board, num) {
+  const [y, x] = position;
   const boardSize = 9;
   // проверка наличия числа в столбце
   for (let i = 0; i < boardSize; i += 1) {
-    if (board[i][col] === number) return false;
+    if (Number(board[y][i] === num && i !== x)) return false;
   }
   // проверка наличия числа в строке
-  for (let j = 0; j < boardSize; j += 1) {
-    if (board[row][j] === number) return false;
+  for (let i = 0; i < boardSize; i += 1) {
+    if (Number(board[i][x] === num && i !== y)) return false;
   }
   // проверка наличия числа в блоке 3х3
-  const blockRowStart = Math.floor(row / 3) * 3;
-  const blockColStart = Math.floor(col / 3) * 3;
+  const blockRowStart = Math.floor(y / 3) * 3;
+  const blockColStart = Math.floor(x / 3) * 3;
 
-  for (let i = blockRowStart; i < 3; i += 1) {
-    for (let j = blockColStart; j < 3; j += 1) {
-      if (board[i][j] === number) return false;
+  for (let i = blockRowStart; i < 3 + blockRowStart; i += 1) {
+    for (let j = blockColStart; j < 3 + blockColStart; j += 1) {
+      if (Number(board[i][j] === num && i !== y && j !== x)) return false;
     }
   }
   return true; // если все 3 проверки прошли то число подходит
 }
 
-// функция заполняет пустые клетки цифрами
-function fillNumber(board) {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  // eslint-disable-next-line no-restricted-syntax
-  for (const number of numbers) {
-    const position = findEmptyField(board);
-    if (checkValidity(number, position, board) === true) {
-      const [i, j] = position;
-      board[i][j] = number;
-    } else fillNumber(board);
+function solve(boardString) {
+  const board = [];
+  const arrBoard = boardString.split("");
+  for (let i = 0; i < arrBoard.length; i += 9) {
+    const check = arrBoard.slice(i, i + 9);
+    board.push(check);
   }
+  // console.log(board);
+  step = () => {
+    const position = findEmptyField(board);
+    if (position === null) {
+      return true;
+    }
+    // console.log(position);
+    for (let num = 1; num <= 9; num += 1) {
+      const isValid = checkValidity(position, board, num);
+      // console.log(isValid);
+
+      if (isValid) {
+        const [y, x] = position;
+        board[y][x] = String(num);
+
+        if (step()) {
+          return true;
+        }
+
+        board[y][x] = "-";
+      }
+    }
+    return false;
+  };
+  step();
   return board;
 }
 
-const proverka = [
-  ["1", "4", "5", "8", "9", "2", "6", "7", "3"],
-  ["8", "9", "3", "1", "7", "6", "4", "2", "5"],
-  ["2", "7", "6", "4", "3", "5", "8", "1", "9"],
-  ["5", "1", "9", "2", "4", "7", "3", "8", "6"],
-  ["7", "6", "2", "5", "8", "3", "1", "9", "4"],
-  ["3", "8", "4", "9", "6", "1", "7", "5", "2"],
-  ["9", "5", "7", "6", "1", "4", "2", "3", "8"],
-  ["4", "3", "8", "7", "2", "9", "5", "6", "1"],
-  ["6", "2", "1", "3", "5", "8", "9", "4", "7"],
-];
+console.log(
+  solve(
+    "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+  )
+);
+
+//  функция ищет первую попавшуюся чёрточку и возвращает её индекс
+// function findEmptyField(board) {
+//   //переменная для результата
+//   const arrPosition = [];
+//   // перебор по массивам
+//   for (let i = 0; i < board.length; i++) {
+//     // поиск элемента с чёрточкой и возврат его индекса
+//     a = board[i].findIndex((el) => el === "-");
+
+//     if (a >= 0) {
+//       //  тут я беру индекс массива в котором есть чёрточки
+//       console.log(board[i]);
+//       arrPosition.push(board.findIndex((el) => el === board[i]));
+//       //  тут я добавляю индекс самой чёрточки
+//       arrPosition.push(a);
+//       // тут была проверка,что нам возвращает результат в консоль
+//       return arrPosition;
+//     }
+//   }
+// }
+// возвращает true или false в зависимости прошла проверка или нет
+
+// функция заполняет пустые клетки цифрами
+// function fillNumber(board) {
+//   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//   // eslint-disable-next-line no-restricted-syntax
+//   for (const number of numbers) {
+//     const position = findEmptyField(board);
+//     if (checkValidity(number, position, board) === true) {
+//       const [i, j] = position;
+//       board[i][j] = number;
+//     } else fillNumber(board);
+//   }
+//   return board;
+// }
+
+// const proverka = [
+//   ["1", "4", "5", "8", "9", "2", "6", "7", "3"],
+//   ["8", "9", "3", "1", "7", "6", "4", "2", "5"],
+//   ["2", "7", "6", "4", "3", "5", "8", "1", "9"],
+//   ["5", "1", "9", "2", "4", "7", "3", "8", "6"],
+//   ["7", "6", "2", "5", "8", "3", "1", "9", "4"],
+//   ["3", "8", "4", "9", "6", "1", "7", "5", "2"],
+//   ["9", "5", "7", "6", "1", "4", "2", "3", "8"],
+//   ["4", "3", "8", "7", "2", "9", "5", "6", "1"],
+//   ["6", "2", "1", "3", "5", "8", "9", "4", "7"],
+// ];
 
 function isSolved(board) {
   let sum = 0;
@@ -128,10 +165,10 @@ function prettyBoard(board) {
     tinycolor.random(),
     tinycolor.random()
   );
-  console.log(coolGradient(veryCoolBoard));
+  // console.log(coolGradient(veryCoolBoard));
   return coolGradient(veryCoolBoard);
 }
-prettyBoard(proverka);
+// prettyBoard(proverka);
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
